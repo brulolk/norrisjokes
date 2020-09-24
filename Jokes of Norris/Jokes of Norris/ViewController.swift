7a2c5901c7c8601a37b724eca0cd0d6310c3a0c7
Bruno Vinicius <brunosiqueira@ioasys.com.br>
2020-09-24 19:18:21 -0300

    initial commit

---
+++ Jokes of Norris/Jokes of Norris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jokes of Norris
+//
+//  Created by Bruno Vinicius on 24/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
